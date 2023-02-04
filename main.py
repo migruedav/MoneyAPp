5a9ec38dff91049a0257b6fd2da23bcecb1c5dca
migruedav <migruedav@hotmail.com>
2023-02-03 19:58:14 -0600

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+cred = credentials.Certificate("/Users/mac/Code/MoneyApp/moneyapp-f3a82-firebase-adminsdk-cuo23-94ccf78707.json")
+try:
+    firebase_admin.initialize_app(cred)
+except:
+    pass
+db = firestore.client()
 
 app = FastAPI()
 
@@ -12,16 +22,30 @@ async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
 
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+@app.get("/home")
+async def home():
+    movimientos = []
+    docs = db.collection(u'movimientos').stream()
 
+    for i in docs:
+        movimiento = {'doc_id':''}
+        movimiento['doc_id']=i.id
+        for k,v in i.to_dict().items():
+            movimiento[k]=v
+        movimientos.append(movimiento)
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
+    cuentas = {'BanortePlatinum':'Banorte Platinum','BanorteClasica':'Banorte Clásica','Efectivo':'Efectivo','Banamex':'Banamex','BanamexRewards':'Banamex Rewards','Nu':'Nu','BBVA':'BBVA'}
 
+    TotalPorCuenta ={}
+    for k,v in cuentas.items():
+        total = (sum([i['monto'] for i in movimientos if ('cuenta' in i)and(i['tipo']=='Ingreso')and(i['cuenta']==v)]))-(sum([i['monto'] for i in movimientos if ('cuenta' in i)and(i['tipo']=='Egreso')and(i['cuenta']==v)]))+(sum([i['monto'] for i in movimientos if ('transcuentaingreso' in i)and(i['transcuentaingreso']==v)]))-(sum([i['monto'] for i in movimientos if ('transcuentaegreso' in i)and(i['transcuentaegreso']==v)]))
+        TotalPorCuenta[k]=total
 
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+    TotalIngresos=sum([i['monto'] for i in movimientos if i['tipo']=='Ingreso'])
+    TotalEgresos=sum([i['monto'] for i in movimientos if i['tipo']=='Egreso'])
+    GranTotal = TotalIngresos-TotalEgresos    
+
+    db.collection('home').document('d7x71RhHQede3VxMuMgN').set(TotalPorCuenta)
+    db.collection('home').document('d7x71RhHQede3VxMuMgN').set({'TotalIngresos':TotalIngresos,'TotalEgresos':TotalEgresos,'GranTotal':GranTotal}, merge=True)
+
+    return "datos del home actualizados"
