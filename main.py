c8a9a268350c9cf4bdb97a383ac7072a823aa591
migruedav <migruedav@hotmail.com>
2023-04-03 10:43:57 -0600

    update main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import json
 import os
 import requests
 
+from gastado import gastado
+
+
 firebase_config_base64 = os.environ.get('firebase64')
 json_data = base64.b64decode(firebase_config_base64).decode('utf-8')
 firebase_config = json.loads(json_data)
@@ -63,37 +66,10 @@ async def home():
     return "Datos del Home actualizados"
 
 @app.get("/gastado")
-async def gastado():
-    today = datetime.datetime.today()
-    firstday = datetime.datetime(today.year, today.month, 1)
-    
-    docs = db.collection('movimientos').where('fecha', '>=', firstday).stream()
-    egresos_por_subcategoria={}
-    for i in docs:
-        if i.to_dict()['tipo'] =='Egreso':
-            if i.to_dict()['subcategoria'] in egresos_por_subcategoria:
-                egresos_por_subcategoria[i.to_dict()['subcategoria']]+=i.to_dict()['monto']
-            else:
-                egresos_por_subcategoria[i.to_dict()['subcategoria']]=i.to_dict()['monto']
+async def get_gastado():
+    data = gastado()
+    return data
 
-
-    docs = db.collection('subcategoriasEgresos').stream()
-
-    for doc in docs:
-        if doc.to_dict()['subcategoria'] not in egresos_por_subcategoria:
-            doc_ref = db.collection('subcategoriasEgresos').document(doc.id)
-            doc_ref.set({'gastado': 0}, merge=True)
-
-    docs = db.collection('subcategoriasEgresos').stream()
-
-    for k, v in egresos_por_subcategoria.items():
-        for doc in docs:
-            if doc.to_dict()['subcategoria'] == k:
-                doc_ref = db.collection('subcategoriasEgresos').document(doc.id)
-                doc_ref.set({'gastado': v}, merge=True)
-
-    
-    return egresos_por_subcategoria
 
 @app.get("/ingresado")
 async def ingresado():
