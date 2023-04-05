ab28da602f0858a7840a4e7cd94b7bddbd72d9ed
migruedav <migruedav@hotmail.com>
2023-04-05 17:48:58 -0600

    update gastado.py and main.py update gastado.py

--- gastado.py
+++ gastado.py
@@ -1,17 +1,43 @@
+import datetime
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+import datetime
+import base64
+import json
+import os
+
+
+firebase_config_base64 = os.environ.get('firebase64')
+json_data = base64.b64decode(firebase_config_base64).decode('utf-8')
+firebase_config = json.loads(json_data)
+
+config = firebase_config
+
+
+cred = credentials.Certificate(config)
+try:
+    firebase_admin.initialize_app(cred)
+except:
+    pass
+db = firestore.client()
+
+
 def gastado():
-    
+
     today = datetime.datetime.today()
     firstday = datetime.datetime(today.year, today.month, 1)
-    
-    docs = db.collection('movimientos').where('fecha', '>=', firstday).stream()
-    egresos_por_subcategoria={}
-    for i in docs:
-        if i.to_dict()['tipo'] =='Egreso':
-            if i.to_dict()['subcategoria'] in egresos_por_subcategoria:
-                egresos_por_subcategoria[i.to_dict()['subcategoria']]+=i.to_dict()['monto']
-            else:
-                egresos_por_subcategoria[i.to_dict()['subcategoria']]=i.to_dict()['monto']
 
+    docs = db.collection('movimientos').where('fecha', '>=', firstday).stream()
+    egresos_por_subcategoria = {}
+    for i in docs:
+        if i.to_dict()['tipo'] == 'Egreso':
+            if i.to_dict()['subcategoria'] in egresos_por_subcategoria:
+                egresos_por_subcategoria[i.to_dict(
+                )['subcategoria']] += i.to_dict()['monto']
+            else:
+                egresos_por_subcategoria[i.to_dict()['subcategoria']] = i.to_dict()[
+                    'monto']
 
     docs = db.collection('subcategoriasEgresos').stream()
 
@@ -25,8 +51,8 @@ def gastado():
     for k, v in egresos_por_subcategoria.items():
         for doc in docs:
             if doc.to_dict()['subcategoria'] == k:
-                doc_ref = db.collection('subcategoriasEgresos').document(doc.id)
+                doc_ref = db.collection(
+                    'subcategoriasEgresos').document(doc.id)
                 doc_ref.set({'gastado': v}, merge=True)
 
-    
     return egresos_por_subcategoria
